f81dbd16634f51f9b9de4f36dafe3514203b0987
maziqing.mzq <maziqing.mzq@alibaba-inc.com>
2023-07-20 17:48:10 +0800

    update exp_main

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -154,8 +154,9 @@ class Exp_Main(Exp_Basic):
                 if (i + 1) % 100 == 0:
                     print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
                     speed = (time.time() - time_now) / iter_count
-                    left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
-                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                    left_time = speed * (train_steps // self.args.batch_size - i)
+                    left_iter = (train_steps // self.args.batch_size - i)
+                    print('\tspeed: {:.4f}s/iter; left iter: {:.4f} iter'.format(speed, left_iter))
                     iter_count = 0
                     time_now = time.time()
 
