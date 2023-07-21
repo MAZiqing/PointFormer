8aca7502e855416c2e5d7ffbb67b095c358fc285
maziqing.mzq <maziqing.mzq@alibaba-inc.com>
2023-07-21 14:32:11 +0800

    fix valid bug

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -111,7 +111,6 @@ class Exp_Main(Exp_Basic):
             self.model.train()
             epoch_time = time.time()
             for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
-                break
                 iter_count += 1
                 model_optim.zero_grad()
                 if i == 0:
